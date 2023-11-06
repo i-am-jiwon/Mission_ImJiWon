a6cdb2be11d91666cf146dca61fe0ebf2a4ff08d
i-am-jiwon <dlark1128@gmail.com>
2023-11-06 09:54:00 +0900

    step1

---
+++ src/main/java/com/ll/textBoard.java
@@ -0,0 +1,17 @@
+package com.ll;
+
+import java.util.Scanner;
+
+public class textBoard {
+    public void run(){
+
+
+        System.out.println("=== 명언앱 ===");
+        System.out.print("명령) ");
+
+        Scanner sc = new Scanner(System.in);
+        String cmd = sc.nextLine();
+
+
+    }
+}
