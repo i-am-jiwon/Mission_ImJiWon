f2c567eb9097ce0fcad88ade1bdc3aa7bcae264b
i-am-jiwon <dlark1128@gmail.com>
2023-11-06 14:59:16 +0900

    step8 refactor cãommand refactor

--- src/main/java/com/ll/base/Command.java
+++ src/main/java/com/ll/base/Command.java
@@ -8,49 +8,44 @@ public class Command {
 
 
     public int cmd(String cmd){
-        String preWords;
         if (cmd.equals("종료")) return 1;
         else if (cmd.equals("등록")) return 2;
         else if (cmd.equals("목록")) return 3;
         else if (cmd.startsWith("삭제")){
-            int index = cmd.indexOf("=");
-            if(index == -1){
-                return 0;
-            }
-            try{
-                preWords = cmd.substring(index-3,index);
-            }catch (StringIndexOutOfBoundsException e){
-                return 0;
-            }
-            if(preWords.equals("?id")){
-                try{
-                    id = Integer.parseInt(cmd.substring(index+1));
-                }catch(NumberFormatException e){
-                    return 0;
-                }
-            }else return 0;
+            int tempId = idSeparator(cmd);
+            if (tempId == 0) return 0;
+            id = tempId;
             return 4;
         }
         else if (cmd.startsWith("수정")){
-            int index = cmd.indexOf("=");
-            if(index == -1){
-                return 0;
-            }
-            try{
-                preWords = cmd.substring(index-3,index);
-            }catch (StringIndexOutOfBoundsException e){
-                return 0;
-            }
-            if(preWords.equals("?id")){
-                try{
-                    id = Integer.parseInt(cmd.substring(index+1));
-                }catch(NumberFormatException e){
-                    return 0;
-                }
-            }else return 0;
+            int tempId = idSeparator(cmd);
+            if (tempId == 0) return 0;
+            id = tempId;
             return 5;
         }
         else return 0;
+    }
+
+    public int idSeparator(String cmd){
+        String preWords;
+
+        int index = cmd.indexOf("=");
+        if(index == -1){
+            return 0;
+        }
+        try{
+            preWords = cmd.substring(index-3,index);
+        }catch (StringIndexOutOfBoundsException e){
+            return 0;
+        }
+        if(preWords.equals("?id")){
+            try{
+                id = Integer.parseInt(cmd.substring(index+1));
+                return id;
+            }catch(NumberFormatException e){
+                return 0;
+            }
+        }else return 0;
     }
 
 }
