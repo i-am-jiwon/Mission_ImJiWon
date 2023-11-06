ee5d00b266d0e587901e39e25082a05424a3cf1e
i-am-jiwon <dlark1128@gmail.com>
2023-11-06 14:25:18 +0900

    step8 error fix

--- src/main/java/com/ll/base/App.java
+++ src/main/java/com/ll/base/App.java
@@ -34,18 +34,35 @@ public class App {
                 case 5 :
                     stor.modify(id);
                     break;
+                default:
+                    System.out.println("*명령어 오류* 다시 입력해 주세요.");
             }
         }
     }
 
     public int cmd(){
         String cmd = sc.nextLine();
+        String preWords;
         if (cmd.equals("종료")) return 1;
         else if (cmd.equals("등록")) return 2;
         else if (cmd.equals("목록")) return 3;
         else if (cmd.startsWith("삭제")){
             int index = cmd.indexOf("=");
-            id = Integer.parseInt(cmd.substring(index+1));
+            if(index == -1){
+                return 0;
+            }
+            try{
+                preWords = cmd.substring(index-3,index);
+            }catch (StringIndexOutOfBoundsException e){
+                return 0;
+            }
+            if(preWords.equals("?id")){
+                try{
+                    id = Integer.parseInt(cmd.substring(index+1));
+                }catch(NumberFormatException e){
+                    return 0;
+                }
+            }else return 0;
             return 4;
         }
         else if (cmd.startsWith("수정")){
