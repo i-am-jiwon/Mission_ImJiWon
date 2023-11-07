ec122a651283978ac087e4c7a79b33d8d2209b60
i-am-jiwon <dlark1128@gmail.com>
2023-11-07 23:46:32 +0900

    file out logic

--- src/main/java/com/ll/domain/SentenceController.java
+++ src/main/java/com/ll/domain/SentenceController.java
@@ -1,7 +1,8 @@
 package com.ll.domain;
 
-import com.ll.base.Command;
-
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -14,7 +15,7 @@ public class SentenceController {
     private Scanner sc;
     Map<Integer, Sentence> authorSentenceMap;
 
-    public SentenceController(Scanner sc){
+    public SentenceController(Scanner sc) {
         this.sc = sc;
         listNum = 0;
         authorSentenceMap = new HashMap<Integer, Sentence>();
@@ -50,33 +51,38 @@ public class SentenceController {
     public void delete(int id) {
         if (authorSentenceMap.containsKey(id)) {
             authorSentenceMap.remove(id);
-            System.out.println(id+"번 명언이 삭제되었습니다.");
-        }
-        else System.out.println(id +"번 명언은 존재하지 않습니다.");
+            System.out.println(id + "번 명언이 삭제되었습니다.");
+        } else System.out.println(id + "번 명언은 존재하지 않습니다.");
     }
 
     public void modify(int id) {
         if (authorSentenceMap.containsKey(id)) {
             Scanner sc = new Scanner(System.in);
-            System.out.println("명언(기존) : "+ authorSentenceMap.get(id).getSentence());
+            System.out.println("명언(기존) : " + authorSentenceMap.get(id).getSentence());
             System.out.print("명언 : ");
             String newSentence = sc.nextLine();
-            System.out.println("작가(기존) : "+ authorSentenceMap.get(id).getAuthor());
+            System.out.println("작가(기존) : " + authorSentenceMap.get(id).getAuthor());
             System.out.print("작가 : ");
             String newAuthor = sc.nextLine();
             authorSentenceMap.get(id).setSentence(newSentence);
             authorSentenceMap.get(id).setAuthor(newAuthor);
-        }
-        else System.out.println(id +"번 명언은 존재하지 않습니다.");
+        } else System.out.println(id + "번 명언은 존재하지 않습니다.");
     }
 
-    public void toFile(){
+    public void toFile() {
+        if (authorSentenceMap.isEmpty()) return;
+
+        File file = new File("file.txt");
+        FileWriter writer;
         for (int i = listNum; i > 0; i--) {
-            // Map에서 있는 것 역순으로 가져오기
-            if (authorSentenceMap.isEmpty()) {
-                return;
-            } else if (authorSentenceMap.containsKey(i)) {
-                System.out.println(i + " / " + authorSentenceMap.get(i).getAuthor() + " / " + authorSentenceMap.get(i).getSentence());
+            if (authorSentenceMap.containsKey(i)) {
+                try{
+                    writer = new FileWriter(file);
+                    writer.write("String to .txt file");
+                    writer.flush();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
             }
         }
     }
